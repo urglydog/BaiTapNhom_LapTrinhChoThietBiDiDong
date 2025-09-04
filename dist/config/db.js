4b3f1a033a9c4e3e97e734aa814e94fcb4f87c09
NguyenChiThien <22667051.thien@student.iuh.edu.vn>
2025-09-05 01:57:02 +0700

    upgrade server

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,10 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-const promise_1 = require("mysql2/promise");
-exports.db = (0, promise_1.createPool)({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "movie_app",
+const promise_1 = __importDefault(require("mysql2/promise"));
+exports.db = promise_1.default.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
